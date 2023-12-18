63e7fc2cd71aa6a1741aa7027d2bd8f2c37afe79
Oliver <hoube14@gmail.com>
2023-12-18 13:33:10 +0100

    fixat local storage och reactions så de funkar och sparas tsm

--- javascript.js
+++ javascript.js
@@ -4,61 +4,56 @@ let submitPostButton = document.getElementById("submitButton");
 
 submitPostButton.addEventListener("click", addNewPost);
 
-let localPosts = localStorage.getItem("posts");
-if (localPosts !== null) {
-    posts = JSON.parse(localPosts);
-    fetchPosts(posts);
-} else {
-    postMessage(posts);
-}
-
-function storePosts(posts) {
+function storePosts() {
+    localStorage.clear();
     localStorage.setItem("posts", JSON.stringify(posts));
-    console.log(localStorage);
 }
-
 
 async function fetchPosts() {
-    // Gör ett anrop mot url och tar emot respons (posts)
     let respons = await fetch("https://dummyjson.com/posts");
-
-    // json "parse:ar" responsen vilket i detta fallet är posts
     respons = await respons.json();
-
-    // Du returnerar din json:fierade response
     return respons.posts;
 }
 
 async function init() {
-    // Tar emot responsen som returneras från fetchPosts i variabeln posts
-    posts = await fetchPosts();
 
-    // anropar och skickar in responsen från fetchPosts()
+    let postsFromLocalStorage = localStorage.getItem("posts");
+    if (postsFromLocalStorage !== null) {
+        let parsedPosts = JSON.parse(postsFromLocalStorage);
+        if (parsedPosts.length > 0) {
+            posts = parsedPosts;
+        } else {
+            posts = await fetchPosts();
+        }
+    } else {
+        posts = await fetchPosts();
+    }
     renderPosts();
+    savePostToLocalStorage();
 }
 
+function savePostToLocalStorage() {
+    localStorage.setItem("posts", JSON.stringify(posts));
+}
 
 function renderPosts() {
-    // Elementet där vi ska trycka in alla våra posts
+
     let mainContainerElement = document.getElementById("main-container");
 
     mainContainerElement.innerHTML = "";
 
-    // Loopar igenom posts arrayen för varje post
+
     for (let i = 0; i < posts.length; i++) {
-        // skapar ett li för varje post i posts arrayn
+
         let liElement = document.createElement("li");
 
-        // Hämtar ut post objektet i posts arrayn med index värdet är i loopen
         let post = posts[i];
 
-        // Fyller vårt nyskapade li element med html med data från post objektet
         liElement.innerHTML = createHtmlPostElement(post, i);
 
-        // Lägger till vårt nyskapade li html element i main-container elementet
         mainContainerElement.prepend(liElement);
     }
-    //
+
 }
 
 
@@ -82,12 +77,15 @@ function addNewPost() {
     };
     posts.push(post);
     renderPosts(posts);
+    storePosts();
+
 }
 
 function likeClicked(postId) {
     let post = posts[postId];
     post.reactions++;
     renderPosts();
+    storePosts();
 }
 
 
